cb132cf00624167b60d5a3a8ff6bc59ca0c29248
Brooks <brooksyang@outlook.com>
2022-04-21 16:07:52 +0800

    add - save and compress file

--- form/file.go
+++ form/file.go
@@ -25,6 +25,23 @@ func SaveUploadedFile(c *gin.Context, file *multipart.FileHeader) (string, error
 		return "", err
 	}
 
+	return path, nil
+}
+
+func SaveAndCompress(c *gin.Context, file *multipart.FileHeader) (string, error) {
+	// Mkdir
+	storagePath := config.NewDir.Upload
+	dirName := fmt.Sprintf("/uploads/%s/", time.Now().Format("2006/01/02"))
+	_ = os.MkdirAll(storagePath+dirName, 0755)
+
+	// Random filename
+	fileName := filePkg.RandomFilename(file)
+	dir := storagePath + dirName
+	path := dir + fileName
+	if err := c.SaveUploadedFile(file, path); err != nil {
+		return "", err
+	}
+
 	if !IsResizable(c, file) {
 		return path, nil
 	}
